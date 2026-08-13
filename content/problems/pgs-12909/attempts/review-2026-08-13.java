6cc19b4cb1e9562f19922e9ae44fb1d88de41325
김민지 <126869805+minzix@users.noreply.github.com>
2026-08-13 11:41:03 +0900

    review: PGS 12909 → UNDERSTOOD

---
+++ content/problems/pgs-12909/attempts/review-2026-08-13.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.add(c);
+            if (c == ')') {
+                if (stack.isEmpty()) answer = false;
+                else stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) answer = false;
+        return answer;
+    }
+}
